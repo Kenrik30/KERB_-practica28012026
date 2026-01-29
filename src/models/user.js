79d7c11c43b5461e07fa263f018418876f313e62
= <=>
2026-01-29 00:13:48 -0600

    Se agrega esquema de la coleecion usuario

---
+++ src/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: { type: String,required: true },
+    nombre: {type: String, required: true},
+    apellido: { type: String, required: true },
+    password: { type: String, required: true },
+    dui: { type: String, required: true }
+});
+
+module.exports = mongoose.model('User', userSchema);
